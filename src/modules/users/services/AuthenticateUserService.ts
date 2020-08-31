af0bd76ea9702507f5599ea67041f35064f06a98
Gabriel antunes <gbr.antunes@gmail.com>
2020-08-31 17:23:50 -0400

    Módulo de testes concluído

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,9 +1,9 @@
-import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 import authConfig from '@config/auth'
 import { injectable, inject } from 'tsyringe'
 
 import IUsersRepository from '../repositories/IUsersRepository'
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
 import AppError from '@shared/errors/AppError'
 
 import User from '../infra/typeorm/entities/User'
@@ -22,7 +22,10 @@ interface IResponse {
 class AuthenticateUserService {
     constructor(
         @inject('UsersRepository')
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -35,7 +38,7 @@ class AuthenticateUserService {
         // user.password = Senha criptografada
         // password = Senha que o usuário usou para tentar logar
 
-        const passwordMatched = await compare(password, user.password)
+        const passwordMatched = await this.hashProvider.compareHash(password, user.password)
 
         if (!passwordMatched) {
             throw new AppError('Incorrect email/password combination', 401)
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,11 +1,11 @@
 import path from 'path'
 import fs from 'fs'
-
 import uploadConfig from '@config/upload'
-
-import IUsersRepository from '../repositories/IUsersRepository'
-import AppError from '@shared/errors/AppError'
 import { injectable, inject } from 'tsyringe'
+
+import AppError from '@shared/errors/AppError'
+import IUsersRepository from '../repositories/IUsersRepository'
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 
 import User from '../infra/typeorm/entities/User'
 
@@ -18,7 +18,10 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -28,18 +31,14 @@ class UpdateUserAvatarService {
       throw new AppError('Only authenticated users can change avatar', 401)
     }
 
+    // Deletar avatar anterior
     if (user.avatar) {
-      // Deletar avatar anterior
-
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await this.storageProvider.deleteFile(user.avatar)
     }
 
-    user.avatar = avatarFilename
+    const filename = await this.storageProvider.saveFile(avatarFilename)
+
+    user.avatar = filename
 
     await this.usersRepository.save(user)
 
